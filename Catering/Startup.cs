c6715dda2f572db164bb77199056433050cd7f8a
velogos <velogos@rambler.ru>
2019-04-08 00:00:20 +0300

    test

--- Catering/Startup.cs
+++ Catering/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
+//lol
 namespace Catering
 {
     public class Startup
